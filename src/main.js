08ac7c1c94f4eecc9022fb1d327147f2eaf41bf4
ferrydjing <ferrydjing@outlook.com>
2018-09-05 16:41:09 +0800

    move router in router dir

--- src/main.js
+++ src/main.js
@@ -2,34 +2,16 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import VueRouter from 'vue-router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-
-import goods from 'components/goods/goods'
-import ratings from 'components/ratings/ratings'
-import seller from 'components/seller/seller'
+import router from './router'
 
 import 'common/stylus/index.styl'
 
-Vue.use(VueRouter)
 // Vue.use(VueResource)
 Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
-
-let routes = [
-    {path: '/goods', component: goods},
-    {path: '/ratings', component: ratings},
-    {path: '/seller', component: seller},
-    {path: '/', redirect: '/goods'}
-]
-
-let router = new VueRouter({
-  // mode: 'history',
-  linkActiveClass: 'active',
-  routes
-})
 
 /* eslint-disable no-new */
 new Vue({
