62d12c3d0a12287823f16a29089ccd5a4af05375
ferrydjing <ferrydjing@outlook.com>
2018-09-05 16:23:44 +0800

    submit router success

---
+++ src/components/v-header/v-header.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="header">
+        <div class="content_wrap">
+            <div class="avatar">
+                <img :src="seller.avatar" width="64" height="64" alt="">
+            </div>
+        </div>
+        <div class="bulletin_wrap"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'v-header',
+    props: {
+        seller: {
+        }
+    }
+}
+</script>
+
+<style lang="stylus">
+</style>
--- src/main.js
+++ src/main.js
@@ -2,14 +2,39 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
+import goods from 'components/goods/goods'
+import ratings from 'components/ratings/ratings'
+import seller from 'components/seller/seller'
+
+import 'common/stylus/index.styl'
+
+Vue.use(VueRouter)
+Vue.use(VueResource)
 
 Vue.config.productionTip = false
+
+let routes = [
+    {path: '/goods', component: goods},
+    {path: '/ratings', component: ratings},
+    {path: '/seller', component: seller},
+    {path: '/', redirect: '/goods'}
+]
+
+let router = new VueRouter({
+  // mode: 'history',
+  linkActiveClass: 'active',
+  routes
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  router,
+  mounted: function () {
+  }
 })
