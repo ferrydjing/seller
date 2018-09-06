ddbfa1ceb32746e10733adc1bfb7bbd9e9a80a93
ferrydjing <ferrydjing@outlook.com>
2018-09-06 17:06:18 +0800

    头部部分完成

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,11 +1,45 @@
 <template>
-    <div>I am goods</div>
+    <div class="goods">
+        <div class="menu-wrapper"></div>
+        <div class="foods-wrappr"></div>
+    </div>
 </template>
 
 <script>
-    export default {}
+    const ERR_OK = 0
+    export default {
+        props: {
+            seller: {
+                type: Object
+            }
+        },
+        data () {
+            return {
+                goods: []
+            }
+        },
+        mounted () {
+            this.axios.get('/api/goods').then((res) => {
+                var data = res.data
+                if (data.errno === ERR_OK) {
+                    this.goods = data.data
+                }
+            })
+        }
+    }
 </script>
 
 <style lang="stylus">
-
+    .goods
+        position absolute
+        display flex
+        top 174px
+        width 100%
+        bottom 46px
+        .menu-wrapper
+            flex 0 0 80px
+            width 80px
+            background #f3f5f7
+        .goods-wrapper
+            flex 1
 </style>
